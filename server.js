c2ca219d78fcd0854fe9d1e3cbff2dcd2ffcb2f8
Ryan Feigum <rpfeigum@gmail.com>
2020-11-23 16:00:23 -0600

    test

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
+app.get('/', (req, res) =>
+    res.json({ msg: 'Welcome to the ContactKeeper API' })
+);
 
 // Define Routes
 app.use('/api/users', require('.routes/users'));
