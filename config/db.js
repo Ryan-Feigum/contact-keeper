6ee5b4b3b463e387538f0949b852eb2db971c11d
Ryan Feigum <rpfeigum@gmail.com>
2021-11-08 22:55:05 -0600

    Updated production json to use correct URI

--- config/db.js
+++ config/db.js
@@ -15,19 +15,6 @@ const connectDB = async () => {
         console.log(err.message);
         process.exit(1);
     }
-
-    const { MongoClient } = require('mongodb');
-    const uri =
-        'mongodb+srv://ryan123:z4nfbSwi9S.Tf^C@contactkeeper.2n4ii.mongodb.net/ContactKeeper?retryWrites=true&w=majority';
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    client.connect((err) => {
-        const collection = client.db('test').collection('devices');
-        // perform actions on the collection object
-        client.close();
-    });
 };
 
 module.exports = connectDB;
