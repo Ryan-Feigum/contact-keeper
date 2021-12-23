0e4785afd11adffc9d63f08a20a0735b82b8a375
Ryan Feigum <rpfeigum@gmail.com>
2021-12-22 22:45:52 -0600

    slight changes

--- routes/auth.js
+++ routes/auth.js
@@ -26,10 +26,10 @@ router.get('/', auth, async (req, res) => {
 // @access   Public
 router.post(
     '/',
-    [
-        check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Password is required').exists(),
-    ],
+
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
